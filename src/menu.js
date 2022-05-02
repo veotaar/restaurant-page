897668c2b797d994254fa7f3c64f97eb9b266c9e
Yalçıncan Ulus <yalcincanulus@gmail.com>
2022-05-02 18:39:51 +0300

    Add menu item generator utility

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,5 @@
+import { menuItem } from "./utils";
+
 export default function menu() {
   const main = document.querySelector("main");
 
@@ -8,6 +10,10 @@ export default function menu() {
   menuItems.textContent = "Menu loaded from menu.js module";
 
   div.appendChild(menuItems);
+
+  div.appendChild(
+    menuItem("Teriyaki Tofu", "Tofu, onions, garlic, teriyaki sauce...", 7)
+  );
 
   main.appendChild(div);
 }
--- src/utils.js
+++ src/utils.js
@@ -13,4 +13,44 @@ function activateTab(tabName) {
   document.querySelector(`[data-tab=${tabName}]`).dataset.active = true;
 }
 
-export { createNavItem, activateTab };
+function menuItem(_dishName, _dishDesc, _price) {
+  /*
+  <div class="menu-item">
+
+    <div class="dish">
+      <p class="dish-name">Teriyaki Tofu</p>
+      <p class="dish-desc">Tofu, onions, garlic, teriyaki sauce...</p>
+    </div>
+
+    <p class="price">7.00 €</p>
+
+  </div>
+*/
+  const item = document.createElement("div");
+  item.classList.add("menu-item");
+
+  const dish = document.createElement("div");
+  dish.classList.add("dish");
+
+  const dishName = document.createElement("p");
+  dishName.classList.add("dish-name");
+  dishName.innerText = _dishName;
+
+  const dishDesc = document.createElement("p");
+  dishDesc.classList.add("dish-desc");
+  dishDesc.innerText = _dishDesc;
+
+  dish.appendChild(dishName);
+  dish.appendChild(dishDesc);
+
+  const price = document.createElement("p");
+  price.classList.add("price");
+  price.innerText = `${_price} €`;
+
+  item.appendChild(dish);
+  item.appendChild(price);
+
+  return item;
+}
+
+export { createNavItem, activateTab, menuItem };
